1c1e0b997d85b59aa397c2f7f2dd0f707571820a
KIMHYEOK\borad <kimmang940715@naver.com>
2025-06-06 15:52:54 +0900

    따배씨 : 강의내용정리

--- ConsoleApplication1/ConsoleApplication1.c
+++ ConsoleApplication1/ConsoleApplication1.c
@@ -6,15 +6,10 @@
 int main()
 {
     // 환율 계산기
+    unsigned int i = 0;
 
-    int won = 0;
-    double dollar = 0;
-
-    printf("Input won \n");
-
-    scanf("%d", &won);
-    dollar = won * 0.00089;
-    printf("Dollar = %f", dollar);
+    sizeof(unsigned int);
+    sizeof(i);
 
     return 0;
 }
